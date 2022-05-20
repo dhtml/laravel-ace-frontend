392c37af5b645614ae07bd7e68ae280f3014e530
Anthony <diltony@gmail.com>
2022-05-20 15:40:55 +0100

    Scaffolded login page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
 import React from 'react'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+
+import Login from './components/login.component'
 
 function App() {
   return (
+    <Router>
+      <div className="App">
+        <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+          <div className="container">
+            <Link className="navbar-brand" to={'/sign-in'}>
+             Laravel Ace
+            </Link>
+            <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+              <ul className="navbar-nav ml-auto">
+              </ul>
+            </div>
+          </div>
+        </nav>
+
+        <div className="auth-wrapper">
+          <div className="auth-inner">
+            <Routes>
+              <Route exact path="/" element={<Login />} />
+            </Routes>
+          </div>
+        </div>
+      </div>
+    </Router>
   )
 }
 
