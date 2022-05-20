0d9bc6bb82208af4d3c1545a2986d31171bcd15f
Anthony <diltony@gmail.com>
2022-05-20 16:04:31 +0100

    Designed table for display

--- src/App.js
+++ src/App.js
@@ -1,33 +1,38 @@
-import React from 'react'
+import React, {useState} from 'react'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 
 import Login from './components/login.component'
+import Dashboard from './components/dashboard.component'
 
 function App() {
+  const [token, setToken] = useState();
+
+  if(token) {
+    return <Login setToken={setToken} />
+  }
+
   return (
     <Router>
       <div className="App">
-        <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+        <nav className="navbar navbar-expand-lg">
           <div className="container">
             <Link className="navbar-brand" to={'/sign-in'}>
              Laravel Ace
             </Link>
-            <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <div className="collapse navbar-collapse" id="navbarToggler">
               <ul className="navbar-nav ml-auto">
               </ul>
             </div>
           </div>
         </nav>
 
-        <div className="auth-wrapper">
-          <div className="auth-inner">
+          <div className="container">
             <Routes>
-              <Route exact path="/" element={<Login />} />
+              <Route exact path="/" element={<Dashboard />} />
             </Routes>
           </div>
-        </div>
       </div>
     </Router>
   )
